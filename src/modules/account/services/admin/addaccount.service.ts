b2ced3bfde32793bfd022b8332aa9553690bae9f
maceteligolden <maceteligolden@gmail.com>
2024-02-19 08:09:50 +0100

    fix:addaccount logic

--- src/modules/account/services/admin/addaccount.service.ts
+++ src/modules/account/services/admin/addaccount.service.ts
@@ -2,6 +2,8 @@ import { injectable } from "tsyringe";
 import { User } from "../../../../common/database/models";
 import { UserRepository } from "../../../../common/database/repositories";
 import { BadRequestError } from "../../../../common/errors";
+import { roles } from "../../../../common/constants";
+import { hash } from "../../../../common/utils";
 
 @injectable()
 export default class AddAccountService {
@@ -19,6 +21,14 @@ export default class AddAccountService {
             throw new BadRequestError("email already exists")
         }
 
-        return await this.userRepository.addUser(args);
+        const encryptedPassword = await hash(args.password ? args.password : "");
+
+        //TODO: send email to user with details
+
+        return await this.userRepository.addUser({
+            ...args,
+            role: roles.ADMIN,
+            password: encryptedPassword
+        });
     }
 }
