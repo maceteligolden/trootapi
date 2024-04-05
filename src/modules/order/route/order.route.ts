e279e322a6b7499bd5e10155be95b1879ac7b31c
maceteligolden <maceteligolden@gmail.com>
2024-04-05 18:36:33 +0100

    feat:add freedownload route

--- src/modules/order/route/order.route.ts
+++ src/modules/order/route/order.route.ts
@@ -7,5 +7,6 @@ const orderController = container.resolve(OrderController);
 
 orderRouter.get("/", (req: Request, res: Response, next: NextFunction)=>orderController.getOrders(req, res, next));
 orderRouter.post("/placeorder", (req: Request, res: Response, next: NextFunction)=>orderController.placeOrder(req, res, next))
+orderRouter.post("/freedownload", (req: Request, res: Response, next: NextFunction)=>orderController.freeDownload(req, res, next))
 
 export default orderRouter;
