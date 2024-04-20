86f497e0cbbad59863e18c6b04fd8c869c603f2a
maceteligolden <maceteligolden@gmail.com>
2024-04-20 18:28:39 +0100

    fix:remove authmiddleware from route

--- src/modules/article/routes/article.route.ts
+++ src/modules/article/routes/article.route.ts
@@ -10,7 +10,7 @@ const articleController = container.resolve(ArticleController);
 articleRouter.post("/add-article", fileMiddleware.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'article', maxCount: 8 }]), (req: Request, res: Response, next: NextFunction)=>articleController.addArticle(req, res, next));
 articleRouter.patch("/update-article", fileMiddleware.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'article', maxCount: 8 }]), (req: Request, res: Response, next: NextFunction)=>articleController.updateArticle(req, res, next));
 articleRouter.delete("/:id", authMiddleware, (req: Request, res: Response, next: NextFunction)=>articleController.deleteArticle(req, res, next));
-articleRouter.get("/:id", authMiddleware, (req: Request, res: Response, next: NextFunction)=>articleController.getArticle(req, res, next));
-articleRouter.get("/", authMiddleware, (req: Request, res: Response, next: NextFunction)=>articleController.getArticles(req, res, next));
+articleRouter.get("/:id", (req: Request, res: Response, next: NextFunction)=>articleController.getArticle(req, res, next));
+articleRouter.get("/", (req: Request, res: Response, next: NextFunction)=>articleController.getArticles(req, res, next));
 
 export default articleRouter;    
