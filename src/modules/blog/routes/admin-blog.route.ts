3a1cf727ed628d7c3169c7e8daa8bf959a84de1a
maceteligolden <maceteligolden@gmail.com>
2024-04-20 17:07:32 +0100

    update:add file middleware to admin blog route

--- src/modules/blog/routes/admin-blog.route.ts
+++ src/modules/blog/routes/admin-blog.route.ts
@@ -8,7 +8,7 @@ const adminBlogRouter = Router();
 const adminBlogController = container.resolve(BlogController);
 
 adminBlogRouter.post("/create",  fileMiddleware.fields([{ name: 'thumbnail', maxCount: 1 }]), (req: Request, res: Response, next: NextFunction)=>adminBlogController.createBlog(req, res, next));
-adminBlogRouter.patch("/:id", (req: Request, res: Response, next: NextFunction) => adminBlogController.updateBlog(req, res, next));
+adminBlogRouter.patch("/:id", fileMiddleware.fields([{ name: 'thumbnail', maxCount: 1 }]),(req: Request, res: Response, next: NextFunction) => adminBlogController.updateBlog(req, res, next));
 adminBlogRouter.delete("/:id", (req: Request, res: Response, next: NextFunction) => adminBlogController.deleteBlog(req, res, next));
 adminBlogRouter.get("/:id", (req: Request, res: Response, next: NextFunction) => adminBlogController.getBlog(req, res, next));
 adminBlogRouter.get("/", (req: Request, res: Response, next: NextFunction) => adminBlogController.getBlogs(req, res, next));
